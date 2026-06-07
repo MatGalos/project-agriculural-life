5335177bce68480ebebb924e2949a27c8395667c
MatGalos <mateuszgalos99@gmail.com>
2026-06-08 01:05:02 +0200

    Implemented app for weather app

--- Scripts/UIs/PlayerHUD/phone_panel.gd
+++ Scripts/UIs/PlayerHUD/phone_panel.gd
@@ -3,15 +3,18 @@ extends Control
 @onready var sell_button: Button = $PanelContainer/MarginContainer/VBoxContainer/BottomNav/HBoxContainer/Sell
 @onready var shop_button: Button = $PanelContainer/MarginContainer/VBoxContainer/BottomNav/HBoxContainer/Shop
 @onready var exchange_button: Button = $PanelContainer/MarginContainer/VBoxContainer/BottomNav/HBoxContainer/StockMarket
+@onready var weather_button: Button = $PanelContainer/MarginContainer/VBoxContainer/BottomNav/HBoxContainer/Weather
 @onready var sell_app: Control = $PanelContainer/MarginContainer/VBoxContainer/ContentArea/PanelContainer/AppContainer/SellApp
 @onready var shop_app: Control = $PanelContainer/MarginContainer/VBoxContainer/ContentArea/PanelContainer/AppContainer/ShopApp
 @onready var exchange_app: Control = $PanelContainer/MarginContainer/VBoxContainer/ContentArea/PanelContainer/AppContainer/StockMarketApp
+@onready var weather_app: Control = $PanelContainer/MarginContainer/VBoxContainer/ContentArea/PanelContainer/AppContainer/WeatherApp
 
 func _ready() -> void:
 	visible = false
 	sell_button.pressed.connect(_on_sell_pressed)
 	shop_button.pressed.connect(_on_shop_pressed)
 	exchange_button.pressed.connect(_on_exchange_pressed)
+	weather_button.pressed.connect(_on_weather_pressed)
 
 func open() -> void:
 	visible = true
@@ -51,3 +54,8 @@ func _on_exchange_pressed() -> void:
 	_hide_all_apps()
 	exchange_app.visible = true
 	exchange_app.refresh()
+
+func _on_weather_pressed() -> void:
+	_hide_all_apps()
+	weather_app.visible = true
+	weather_app.refresh()
